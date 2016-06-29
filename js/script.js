73c2393a6b6da618138532f0645009275fe26143
Pancho <panchenko923@gmail.com>
2016-06-30 01:25:50 +0300

    отрисованы лунки

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 
 
-function Game (container) {
-	this.container = document.getElementsByClassName('container')[0];
+function Game (options) {
+
+	var self = this;
+
+	options.numberOfHoles = options.numberOfHoles;
+	options.container = options.container;
+
+	if (!options.numberOfHoles) {
+		options.numberOfHoles = 3;
+	}
+
+	this.init = function () {
+		this.drawHoles();
+	}
+
+	this.drawHoles = function () {
+		var container = document.getElementsByClassName(options.container)[0];
+
+		for(var i = 0; i < options.numberOfHoles; i++) {
+			var hole = document.createElement('div');
+			hole.className = 'hole';
+			container.appendChild(hole);
+		}
+	}
 }
