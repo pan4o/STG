72a0e674b34ae332501a169d4d2f7729a2507c24
Pancho <panchenko923@gmail.com>
2016-07-01 02:11:24 +0300

    small fixes

--- js/script.js
+++ js/script.js
@@ -4,24 +4,25 @@ function Game (options) {
 
 	var self = this;
 
-	options.numberOfHoles = options.numberOfHoles;
-	options.container = options.container;
-
-	if (!options.numberOfHoles) {
-		options.numberOfHoles = 3;
-	}
+	this.numberOfHoles = options.numberOfHoles || 3;
+	this.container = document.getElementsByClassName(options.container)[0];
+	this.holesArr = [];
 
 	this.init = function () {
 		this.drawHoles();
 	}
 
 	this.drawHoles = function () {
-		var container = document.getElementsByClassName(options.container)[0];
-
-		for(var i = 0; i < options.numberOfHoles; i++) {
+		for(var i = 0; i < this.numberOfHoles; i++) {
 			var hole = document.createElement('div');
 			hole.className = 'hole';
-			container.appendChild(hole);
+			this.container.appendChild(hole);
+			this.holesArr.push(hole);
 		}
 	}
+
+	this.setHolePosition = function () {
+
+	}
+
 }
